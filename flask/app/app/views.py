9f9fe776eafd4a6409b9bcade1b6042af9ffeb98
mtk920 <matsu9.tigers20@gmail.com>
2022-08-20 19:24:03 +0900

    feat ルート制御

--- flask/app/app/views.py
+++ flask/app/app/views.py
@@ -1,23 +1,7 @@
 from flask import render_template
-from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import BaseView, expose, has_access, ModelView, ModelRestApi
+from . import main
 
-from . import appbuilder, db
-
-
-class MyView(BaseView):
-
-    default_view = 'method1'
-
-    @expose('/method1/')
-    @has_access
-    def method1(self):
-        # do something with param1
-        # and return to previous page or index
-
-        return 'Hello'
-
-
-appbuilder.add_view(MyView, "Method1", category='My View')
-
-db.create_all()
+@main.route('/', method=['GET', 'POST'])
+def index():
+    print('!!!')
+    return render_template('index.html')
